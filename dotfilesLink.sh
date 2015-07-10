f9c0a6ea810c5b2f256693c441ae7109a04746b4
Reo ASANO <reo@SR2-REO-MBP.local>
2015-07-10 09:44:36 +0900

    add .git-completion.bash

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,4 +2,4 @@
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
 ln -sf ~/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-
+ln -sf ~/dotfiles/.git-completion.bash ~/.git-completion.bash
