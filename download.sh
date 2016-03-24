e67787ff5d1288898552926601309e076d54b302
Reo ASANO <reo@SR2-REO-MBP.local>
2016-03-24 23:44:43 +0900

    fix url

--- download.sh
+++ download.sh
@@ -2,8 +2,8 @@ mkdir solarized
 mkdir git-completion
 
 cd solarized
-curl -L -O https://github.com/seebi/dircolors-solarized/blob/master/dircolors.ansi-dark
-curl -L -O https://github.com/altercation/vim-colors-solarized/blob/master/colors/solarized.vim
+curl -L -O https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
+curl -L -O https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
 
 cd ../git-completion
-curl -L -O https://github.com/git/git/blob/master/contrib/completion/git-completion.bash
+curl -L -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
