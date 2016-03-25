308632af3523733d88863fd7f0d97413fce8c94c
Reo ASANO <reo@SR2-REO-MBP.local>
2016-03-25 23:37:16 +0900

    rename

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/git-completion/git-completion.bash ~/.git-completion.bash
+
+cp wifi-signal-strength /usr/local/bin/
+chmod +x /usr/local/bin/wifi-signal-strength
--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-
 unlink ~/.bashrc
 unlink ~/.bash_profile
 unlink ~/.inputrc
 unlink ~/.tmux.conf
 unlink ~/.vimrc
 unlink ~/.git-completion.bash
+
+rm -f /usr/local/bin/wifi-signal-strength
