84a03c0274a30ac69aa57b5bf887567720adaffd
Reo ASANO <reo@SR2-REO-MBP.local>
2016-03-25 23:28:25 +0900

    add bin dir for battery and wifi

--- download.sh
+++ download.sh
@@ -1,5 +1,10 @@
+rm -rf solarized
+rm -rf git-completion
+rm -rf bin
+
 mkdir solarized
 mkdir git-completion
+mkdir bin
 
 cd solarized
 curl -L -O https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
@@ -7,3 +12,7 @@ curl -L -O https://raw.githubusercontent.com/altercation/vim-colors-solarized/ma
 
 cd ../git-completion
 curl -L -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+
+cd ../bin
+curl -L -O https://raw.githubusercontent.com/carsonmcdonald/tmux-wifi-os-x/master/wifi-signal-strength
+git clone https://github.com/tmux-plugins/tmux-battery
