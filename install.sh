c12a2a17f594a376b42e5c1f3d42ba7db9b51378
Reo ASANO <reo@SR2-REO-MBP.local>
2017-04-18 17:42:23 +0900

    remove bash-completion

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/git-completion/git-completion.bash ~/.git-completion.bash
 
 # tmux
 cp bin/wifi-signal-strength /usr/local/bin/
